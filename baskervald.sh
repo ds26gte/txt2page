ee6ff0706c71364143d5bd6814c2ad7f1dfa9af6
Dorai Sitaram <ds26gte@yahoo.com>
2014-10-01 10:54:29 -0400

    ...

--- baskervald.sh
+++
@@ -1,7 +0,0 @@
-# last change 2011-11-12
-
-otf2fam.sh baskervald \
-    BaskervaldADFStd.otf \
-    BaskervaldADFStd-Italic.otf \
-    BaskervaldADFStd-Bold.otf \
-    BaskervaldADFStd-BoldItalic.otf
